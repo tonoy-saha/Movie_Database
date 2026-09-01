086e3da6a60dcb48fc253513ec4750889a78cdce
tonoy-saha <tonoysaha180@gmail.com>
2026-09-01 23:42:32 +0600

    added clickable button

--- app/src/main/java/com/example/tmdb/MainActivity.java
+++ app/src/main/java/com/example/tmdb/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.tmdb;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+    private LinearLayout navHome, navDiscover, navLibrary, navProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,42 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        navHome = findViewById(R.id.navHome);
+        navDiscover = findViewById(R.id.navDiscover);
+        navLibrary = findViewById(R.id.navLibrary);
+        navProfile = findViewById(R.id.navProfile);
+
+        loadFragment(new HomeFragment());
+
+        navHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new HomeFragment());
+            }
+        });
+        navDiscover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new DiscoverFragment());
+            }
+        });
+        navLibrary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new LibraryFragment());
+            }
+        });
+        navProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new ProfileFragment());
+            }
+        });
+    }
+    private void loadFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commitAllowingStateLoss();
     }
 }
